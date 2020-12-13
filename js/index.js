6222b94f03076103ed7f65955ae5219d541baea7
dailycoding1 <souvikm111@gmail.com>
2020-12-13 21:10:21 +0530

    Learn Coding & Develop your skill

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+window.onload = function () {
+    /* console.log("loaded") */
+    var typed = new Typed('#typed', {
+        strings: ["Learn Front End Development",
+      "Back End Development",
+      "And also SharePoint Administrator",
+      "And much more coding with us :)"],
+        backSpeed: 15,
+        smartBackspace: false,
+        backDelay: 1200,
+        startDelay: 1000,
+        typeSpeed: 50,
+        loop: true,
+    });
+};
